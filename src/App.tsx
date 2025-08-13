ae6b68c9d2a54e1c8d9e1a1eede868f6f926a0a3
TezanaK <kjntester@gmail.com>
2025-08-13 08:39:18 +0200

    Internal Linking SEO Optimization

--- src/App.tsx
+++ src/App.tsx
@@ -615,14 +615,4 @@ function App() {
         <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-24 text-white">
           <div className="text-center">
             <div className="flex justify-center mb-6">
-              <div className="glass-dark rounded-2xl px-6 py-3 border border-white/20">
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
-}
-
-export default App;
+              <div className="glass-dark rounded-2xl px-6 py-3 border
